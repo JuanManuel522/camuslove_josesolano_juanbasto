5dded232e54806da3461051a9e9a48aae6092f42
JuanManuel522 <194614495+JuanManuel522@users.noreply.github.com.>
2025-08-25 19:49:11 -0500

    feat: :sparkles: Corrigiendo ultimos detalles

--- obj/Debug/net9.0/campuslove.AssemblyInfo.cs
+++ obj/Debug/net9.0/campuslove.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("campuslove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da24bee4750abb0487f8eb679f8501d76a02f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62eb6d12f77c76e1b115f9fc3ea4eb2fa1fe1caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("campuslove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("campuslove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
